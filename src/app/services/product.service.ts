d8f6ad39e677990cb816453d1e6b70cea845c560
Waihon Yew <yewwaihon@gmail.com>
2022-01-21 22:13:34 +0800

    Update product service to call REST APIs

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  // Unwraps the JSON from Spring Data REST _embedded entry
+  _embedded: {
+    products: Product[];
+  }
 }
