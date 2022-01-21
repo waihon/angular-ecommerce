8f319ab746eeeba132a9635d9c20ea94e5bc7d76
Waihon Yew <yewwaihon@gmail.com>
2022-01-21 22:49:46 +0800

    Fix product properties have no initializer and are not assigned in the constructor

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,11 +1,11 @@
 export class Product {
-  sku: string;
-  name: string;
-  description: string;
-  unitPrice: number;
-  imageUrl: string;
-  active: boolean;
-  unitsInStock: number;
-  dateCreated: Date;
-  lastUpdated: Date;
+  sku: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  unitPrice: number | undefined;
+  imageUrl: string | undefined;
+  active: boolean | undefined;
+  unitsInStock: number | undefined;
+  dateCreated: Date | undefined;
+  lastUpdated: Date | undefined;
 }
